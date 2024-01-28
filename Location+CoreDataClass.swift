9c9019a7583eb0134584f91ae604e1fde939cee7
Julia <julia.gurbanova@gmail.com>
2024-01-28 20:32:55 +0300

    added heads-up display, saving locations using CoreData

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Julia Gurbanova on 28.01.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Julia Gurbanova on 07.01.2024.
 //
 
+import CoreData
 import CoreLocation
 import UIKit
 
@@ -28,10 +29,22 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
 
     var timer: Timer?
 
+    var managedObjectContext: NSManagedObjectContext!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         updateLabels()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
     }
 
     // MARK: - Actions
@@ -242,6 +255,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             stopLocationManager()
             lastLocationError = NSError(domain: "MyLocationsErrorDomain", code: 1, userInfo: nil)
             updateLabels()
+        }
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+            controller.managedObjectContext = managedObjectContext
         }
     }
 }
